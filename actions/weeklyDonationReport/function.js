fb66babd4e2b97167bf62c03970ee8599838d30c
Luke Andrews <luke@attaboy.ca>
2019-05-07 20:38:22 +0000

    Use new uploadFromUrl to ensure charts as files to Slack

--- actions/weeklyDonationReport/function.js
+++ actions/weeklyDonationReport/function.js
@@ -1,7 +1,8 @@
 function(ellipsis) {
   const greeting = require('ellipsis-random-response').greetingForTimeZone(ellipsis.team.timeZone);
 const moment = require('moment-timezone');
-const today = moment.tz(ellipsis.team.timeZone).format("M/D/YYYY");
+const now = moment.tz(ellipsis.team.timeZone);
+const today = now.format("M/D/YYYY");
 const {client, sheets} = require('ellipsis_google_sheets_api')(ellipsis);
 
 const SPREADSHEET_ID = "1xXGQThvw7fHtqzlagWh0JMXPH43emYeeYrCDXKK1TqM";
@@ -38,7 +39,10 @@ client.authorize().then(() => {
     const seedlingData = rowsWithData.map((row) => Number.parseFloat(row[SEEDLING_TOTAL_INDEX]));
     const chartData = getChartData(dates, produceData, seedlingData);
     const chartUrl = encodeURI(`https://quickchart.io/chart?c=${JSON.stringify(chartData)}&backgroundColor=white&width=500&height=300`);
-    ellipsis.success(`
+    return ellipsis.uploadFromUrl(chartUrl, {
+      filename: `donation-tracking-chart-${now.format("YYYY-MM-DD")}`
+    }).then((uploadedUrl) => {
+      ellipsis.success(`
 ${greeting}
 
 Here is the donation report for the week of ${date}.
@@ -52,11 +56,11 @@ Total trays: **${firstRow[TOTAL_TRAYS_INDEX] || "(unknown)"}**
 Total seedlings: **${firstRow[TOTAL_SEEDLINGS_INDEX] || "(unknown)"}**
 Total produce: **${firstRow[TOTAL_PRODUCE_INDEX] || "(unknown)"} lb**
 
-[Donation tracker as of ${today}](${chartUrl})
+[Donation tracker as of ${today}](${uploadedUrl})
 `);
+    });
   }
 });
-
 function rowToDate(row) {
   const dateCol = row[DATE_COLUMN_INDEX];
   const date = dateCol ? moment(dateCol, ["M/D/YY", "M/D/YYYY", "MM/DD/YYYY"]) : null;
@@ -81,6 +85,7 @@ function getChartData(dates, produceData, seedlingData) {
       }, {
         label: 'Seedlings', 
         data: seedlingData,
+        borderColor: 'hsl(127, 75%, 48%)',
         backgroundColor: 'hsl(127, 75%, 48%)',
         yAxisID: 'seedlings'
       }]
